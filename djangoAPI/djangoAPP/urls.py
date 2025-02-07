07db08a370084377257b681a84c369ab7509ed1d
anggierz <anggie.rizo.zambrano@gmail.com>
2025-02-07 20:07:27 +0100

    add configurations

---
+++ djangoAPI/djangoAPP/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+
+urlpatterns = [
+     # path('stock/<str:symbol>/', StockAPIView.as_view(), name='stock-detail'),
+     # path('stock/', StockListCreateView.as_view(), name='stock-create'),
+]
