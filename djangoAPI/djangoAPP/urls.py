c65d9e1f3097ca79de213a820a90428bc55c91d8
anggierz <anggie.rizo.zambrano@gmail.com>
2025-02-07 21:59:36 +0100

    USER CRUD OPERATIONS OK

--- djangoAPI/djangoAPP/urls.py
+++ djangoAPI/djangoAPP/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import get_users
+from .views import get_users, get_user_by_id, create_user, update_user, delete_user
 
 
 urlpatterns = [
-     path('users/', get_users, name='get-users')
+     path('users/', get_users, name='get-users'),
      # path('stock/', StockListCreateView.as_view(), name='stock-create'),
+     path('users/<int:id>', get_user_by_id, name='get-user-by-id'),
+     path('users/create/', create_user, name='create-user'),
+     path('users/update/<int:id>', update_user, name='update-user'),
+     path('users/delete/<int:id>', delete_user, name='delete-user'),
 ]
--- djangoAPI/djangoAPP/views.py
+++ djangoAPI/djangoAPP/views.py
@@ -9,6 +9,62 @@ from .models import User
 
 # --------------------------- USER CRUD OPERATIONS --------------------------
 
+#Get all users
 @api_view(['GET'])
 def get_users(request):
     return Response(User.objects.all(), status=status.HTTP_200_OK)
+
+#Get User By Id
+@api_view(['GET'])
+def get_user_by_id(request, id):
+    user = User.objects.filter(id=id)
+    if not user.exists():
+        return Response({'Error': f"User with id {id} not found in database"}, status=status.HTTP_404_NOT_FOUND)
+    
+    return Response(user, status=status.HTTP_200_OK)
+
+
+#Create User
+@api_view(['POST'])
+def create_user(request):
+    serializer = UserSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#Update User https://medium.com/@altafkhan_24475/patch-method-of-apiview-in-django-rest-framework-e7c0d574a47f
+@api_view(['PATCH'])
+def update_user(request, id):
+    try:
+        #Retrieve the item by Id 
+        user_retrieved = User.objects.get(id=id) 
+    except User.DoesNotExist:
+        return Response({"Error": f"User with {id} not found"}, status=status.HTTP_404_NOT_FOUND)
+            
+     #Partially update with incoming data serializer Itemserializer(item, data-request.data, partial-True)
+    serializer = UserSerializer(user_retrieved, data=request.data, partial=True)
+    if serializer.is_valid():
+        #Save only the fields provided 
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    else:
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+
+#Delete User
+@api_view(['DELETE'])
+def delete_user(request, id):
+    try:
+        #Retrieve the item by Id 
+        user_retrieved = User.objects.get(id=id) 
+    except User.DoesNotExist:
+        return Response({"Error": f"User with {id} not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    user_retrieved.delete()
+    
+    return Response({"Response": f"User deletion completed successfully"}, status=status.HTTP_200_OK)
